7d080a996cd22f4e193777864b5531a5c60e633a
Jabahahaha <andredrummondthegoat@gmail.com>
2026-03-03 09:09:34 +0700

    Install Laravel Breeze with Blade authentication scaffolding

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center gap-2" {{ $attributes }}>
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    <x-app-logo-icon class="size-5 fill-current text-white" />
     <span class="font-semibold">Crypto Exchange</span>
 </div>
